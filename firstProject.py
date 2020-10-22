937caa2403ad843eb81e7a33be7670a1aab3fe19
Nokia Bird <willynillyy@protonmail.com>
2020-10-21 22:31:20 -0400

    some bro talk

--- firstProject.py
+++ firstProject.py
@@ -1,2 +1,10 @@
 # First Project of the automate the boring stuff with python.
 # Setting up with the laptop.
+# Just updating stuff
+coder = "Young adult"
+wasting_life = True
+
+if coder == "Young adult" and wasting_life:
+	print("Please stop wasting your precious young life")
+else:
+	print("keep up the good work")
